f93c6aa10afa8bfab7830ddafb95b409c8aabfa6
Sebastian Bachmann <me@free-minds.net>
2014-06-19 08:32:57 +0200

    fix line breaker in run_once script

--- run_once.sh
+++ run_once.sh
@@ -2,7 +2,6 @@
 
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 8
-200
+xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 8 200
 xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
 
